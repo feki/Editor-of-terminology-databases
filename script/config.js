9a0a1a35a4153e202a0a59ee4ef69b367384bda4
Ján Gábela <jan.gabela@gmail.com>
2011-11-15 00:35:13 +0100

    initial project version

---
+++ script/config.js
@@ -0,0 +1,10 @@
+/**
+ * Vzorovy konfiguracny pre potreby projektu
+ */
+config = {
+	"name": "", // meno terminologickej databazy 
+	"input": {
+		"type": "checkbox",
+		"values": [["prefinal", "predfinalny"], ["final", ["finalny"]]]
+	}
+}
---
+++ script/process_configuration.js
@@ -0,0 +1,8 @@
+/**
+ * Konstruktor pre configuration objekt
+ */
+var configuration = function(config) {
+	var that = {};
+	
+	var dom = JAK.DOM; // dokument objekt s 
+}
